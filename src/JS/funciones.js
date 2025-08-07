f7e99de530e0a0759081140ab03290c94e16da58
AntthonyQQ <aquiangquiros2025fwd@gmail.com>
2025-08-07 09:49:37 -0600

    funciones medio hechas

---
+++ src/JS/funciones.js
@@ -0,0 +1,18 @@
+
+
+const precio = document.getElementById("precio")
+const producto = document.getElementById("producto")
+const categoria = document.getElementById("categoria")
+
+
+
+function containerbuilder()
+{
+
+}
+
+
+
+//divs father verduras
+// frutas
+// otros 
